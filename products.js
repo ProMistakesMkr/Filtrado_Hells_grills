b879624852ca7499ba5b7f6836a5eaa45cd199fa
Santiago Granobles <santiago.granobles@hotmail.com>
2022-11-02 04:31:16 -0500

    Filtrado

---
+++ products.js
@@ -0,0 +1,292 @@
+const productsList = [
+  {
+    num: 1,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/baca7312-6f17-4eb1-bd24-e20e97d8b37c?quality=60&_mzcb=_1649148331752",
+    item: "Weber Genesis Burner",
+    price: "$1.129",
+    material: "Reinforced Stainless Steel",
+    tipo: "Wood Pellet",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 2,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/1cd94488-c261-4c18-b11d-57d44162ed6a?quality=60&_mzcb=_1649148331752",
+    item: "Weber Genesis 3 Burner",
+    price: "$1.129",
+    material: "Iron",
+    tipo: "Wood Pellet",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 3,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/3cde9599-ad7e-4fb1-a992-e1806970c288?quality=60&_mzcb=_1649148331752",
+    item: "Weber Genesis SP-E 3 Burner",
+    price: "$1.129",
+    material: "Iron",
+    tipo: "Wood Pellet",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 4,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/66b777f8-b937-4728-8bcf-623e27a8b8dd?quality=60&_mzcb=_1649148331752",
+    item: "Weber Genesis S-325-E 3 Burner",
+    price: "$1.129",
+    material: "Iron",
+    tipo: "Wood Pellet",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 5,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/eee252df-7e2d-46da-8d50-0a2900d2d635?quality=60&_mzcb=_1649148331752",
+    item: "Weber Genesis E-565 3 Burner",
+    price: "$1.129",
+    material: "Iron",
+    tipo: "Bark",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 6,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/e519a26f-657d-4d84-b4e4-706f71faf100?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Timberline",
+    price: "$3.499",
+    material: "Iron",
+    tipo: "Wood Pellet",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 7,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/c31530ec-bf27-4670-a0ab-17aa50d16894?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Ironwood",
+    price: "$1.599",
+    material: "Iron",
+    tipo: "Wood Pellet",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 8,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/2da09995-9cd3-4619-a977-86818d2e376d?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Timberline 850",
+    price: "$2.099",
+    material: "Stainless Steel",
+    tipo: "Soy Pellet",
+    tipo2: "Inside",
+  },
+
+  {
+    num: 9,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/b1f12289-846d-48da-adf7-0985bb74ee33?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Timberline 1300",
+    price: "$2.299",
+    material: "Reinforced Stainless Steel",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 10,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/8dc84d84-2e27-400a-a1b9-92ac3236a68c?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Pro Series 34",
+    price: "$749",
+    material: "Stainless Steel",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 11,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/aee1181e-8cc3-42c3-8817-b9283841c3de?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Pro 575",
+    price: "$899",
+    material: "Stainless Steel",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 12,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/fc6e4e37-e1db-4ee0-b8b8-9a6424111525?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Pro 780",
+    price: "$999",
+    material: "Stainless Steel",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 13,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/1d1f01c5-4cde-47d1-bace-12a6fed2bb0b?quality=60&_mzcb=_1649148331752",
+    item: "Weber 22 Gray",
+    price: "$439",
+    material: "Stainless Steel",
+    tipo: "Wood Pellet",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 14,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/f9ac6352-c8e7-4acb-a86f-d3ef236f3bbd?quality=60&_mzcb=_1649148331752",
+    item: "Weber 22 Yellow",
+    price: "$439",
+    material: "Stainless Steel",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 15,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/8b6f5211-abf9-44ff-87d8-d36cc266bf7d?quality=60&_mzcb=_1649148331752",
+    item: "Big Green Egg",
+    price: "$1.364",
+    material: "Iron",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 16,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/36fa44f4-f538-48be-8b4c-ecd447048299?quality=60&_mzcb=_1649148331752",
+    item: "Traeger Portable Grill",
+    price: "$499",
+    material: "Stainless Steel",
+    tipo: "Electric",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 17,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/e3731abb-6644-4ced-9c84-ef31a384e2ae?quality=60&_mzcb=_1649148331752",
+    item: "Ooni Karu 16",
+    price: "$799",
+    material: "Iron",
+    tipo: "All Purpose",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 18,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/67a6556f-ce6c-4dbd-8bff-7fe28e568ea8?quality=60&_mzcb=_1649148331752",
+    item: "Ooni Pro 16",
+    price: "$599",
+    material: "Stainless Steel",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 19,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/f4a0349a-2506-479f-b60e-6d2c2a88e766?quality=60&_mzcb=_1649148331752",
+    item: "Ooni Koda 12",
+    price: "$399",
+    material: "Stainless Steel",
+    tipo: "Mixed",
+    tipo2: "Outside",
+  },
+
+  {
+    num: 20,
+    imageProduct:
+      "https://cdn-tp3.mozu.com/24645-37138/cms/37138/files/48d4e755-8365-4fb8-a699-eec83ec997b5?quality=60&_mzcb=_1649148331752",
+    item: "Ooni Karu 12",
+    price: "$399",
+    material: "Stainless Steel",
+    tipo: "Coal",
+    tipo2: "Inside",
+  },
+];
+
+const filter_fuel_type = document.getElementById("filter_fuel_type");
+const filter_material_type = document.getElementById("filter_material_type");
+const filter_placing = document.getElementById("filter_placing");
+
+filter_fuel_type.addEventListener("change", (e) => {
+  filtrado();
+});
+filter_material_type.addEventListener("change", (e) => {
+  filtrado();
+});
+filter_placing.addEventListener("change", (e) => {
+  filtrado();
+});
+
+function filtrado() {
+  let filtradoDeProductos = [...productsList];
+  const fuel_type = filter_fuel_type.value || "";
+  const material_type = filter_material_type.value || "";
+  const placing = filter_placing.value || "";
+  productosDestacados.innerHTML = "";
+
+  let filtros = {};
+
+  if (fuel_type === "fuel_type" || fuel_type === "All") {
+    filtrado_productos;
+  } else {
+    filtros["fuel_type"] = fuel_type;
+  }
+
+  if (fuel_type === "MATERIAL" || material_type === "All") {
+    filtrado_productos;
+  } else {
+    filtros["material"] = material;
+  }
+
+  if (placing === "PLACING" || placing === "All") {
+    filtrado_productos;
+  } else {
+    filtros["placing"] = placing;
+  }
+
+  const filtracion = filtradoDeProductos.filter((p) => {
+    return (
+      (filtros["fuel_type"] ? p.tipo === filtros["fuel_type"] : true) &&
+      (filtros["material"] ? p.material === filtros["material"] : true) &&
+      (filtros["placing"] ? p.tipo2 === filtros["placing"] : true)
+    );
+  });
+
+  mostrar(filtracion);
+  mostrar();
+}
+
+const productosDestacados = document.getElementById("productos_destacados");
+let filtrado_productos = productsList;
+mostrar(productsList);
+
+function mostrar(productsList) {
+  productsList.forEach((product) => {
+    productosDestacados.innerHTML += `<a href= './item.html?id=${product.num}' class="card_fil">
+        <figure class= "card_figure"><img class= "card_img" src="${product.imageProduct}"></figure>
+        <article class="card_article">
+            <h2>${product.item}</h2>
+            <p>${product.tipo}</p>
+            <h3 class="texto_precio">${product.price}</h3>
+            <h5> ${product.material}</h5>
+        </article>
+    </a>`;
+  });
+}
